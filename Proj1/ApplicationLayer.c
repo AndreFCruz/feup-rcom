3fd97fa2fbf36ca92181cae0ea8fbf03ff729f79
root <root@bancada4.netlab.fe.up.pt>
2017-10-13 16:28:40 +0100

    :8ball:

---
+++ Proj1/ApplicationLayer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ApplicationLayer.h"
+
+
+int sendFile() {
+  return 0;
+}
+
+int receiveFile() {
+  return 0;
+}
--- Proj1/ApplicationLayer.h
+++ Proj1/ApplicationLayer.h
@@ -5,10 +5,9 @@
 
 #define MAX_FILE_NAME 255
 
-
 typedef struct {
   int fileDescriptor; // serial port's file descriptor
-  //ConnectionType status;
+  ConnectionType status;
 } ApplicationLayer;
 
 typedef struct {
@@ -17,13 +16,25 @@ typedef struct {
 } Packet;
 
 typedef struct {
+  unsigned char seqNr;
+  unsigned int packetSize;
+  unsigned char * data;
+} DataPacket;
+
+typedef struct {
 	int fileSize;
 	char fileName[MAX_FILE_NAME];
 	int argNr;
-} ControlPacketInf;
+} ControlPacket;
 
-int sendDataPacket(Packet * packet);
-int sendCmdPacket(Packet * packet);
+int sendFile();
+int receiveFile();
+
+int sendControlPacket(ControlPacket * src);
+int receiveControlPacket(ControlPacket * dest);
+
+Packet * sendDataPacket(DataPacket * packet);
+Packet * receiveDataPacket(DataPacket * packet);
 
 
 #endif
