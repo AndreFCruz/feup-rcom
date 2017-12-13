fbd21f98bddecb3675231dcfe2bf51ac70ecf563
AndreFCruz <andrecruz97@gmail.com>
2017-12-13 15:50:23 +0000

    bubacar

--- Proj2/src/Url.h
+++ Proj2/src/Url.h
@@ -1,34 +1,41 @@
 #ifndef __URL_H
 #define __URL_H
 
+#define URL_STR_LEN 256
+
 #include <string.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <regex.h>
 #include <errno.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include <arpa/inet.h>
-
 #include <netinet/in.h>
 
-typedef char url_content[256];
-
 typedef struct url_t {
-  int port; // integer to port
-  url_content ip; // string to IP
-  url_content host; // string to host
-  url_content path; // string to path
-  url_content filename; // string to filename
-  url_content user; // string to user
-  url_content password; // string to password
+  int port;       // integer to port
+  char ip[URL_STR_LEN];   // string to IP
+  char host[URL_STR_LEN]; // string to host
+  char path[URL_STR_LEN]; // string to path
+  char filename[URL_STR_LEN]; // string to filename
+  char user[URL_STR_LEN]; // string to user
+  char password[URL_STR_LEN]; // string to password
 } URL;
 
-void constructURL();
-int parseURL(URL* url, const char* str); // Parse a string with the url to create the URL structure
-void destructURL();
+/**
+ * URL struct constructor
+ */
+URL * constructURL();
+
+/**
+ * Parses the url command to the given URL struct
+ */
+int parseURL(URL* url, const char* str);
+
+/**
+ * URL struct destructor
+ */
+void destructURL(URL * url);
 
 #endif /* __URL_H */
