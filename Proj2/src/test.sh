a1203753d973df3299a8b18fac500bcc826f14b0
AndreFCruz <andrecruz97@gmail.com>
2017-12-14 22:33:28 +0000

    minor change

---
+++ Proj2/src/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./clientFTP ftp://demo:password@test.rebex.net/pub/example/mail-editor.png
+
+./clientFTP ftp://ftp.up.pt/pub/CTAN/timestamp
+
+./clientFTP ftp://speedtest.tele2.net/20MB.zip
